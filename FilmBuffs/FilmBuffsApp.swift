7faafe833839f0a396211ad3dc34366486ed25ae
Clay Beal <125436817+clayster4004@users.noreply.github.com>
2024-11-05 12:40:57 -0500

    Initial Commit

---
+++ FilmBuffs/FilmBuffsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilmBuffsApp.swift
+//  FilmBuffs
+//
+//  Created by Clay Beal on 11/5/24.
+//
+
+import SwiftUI
+
+@main
+struct FilmBuffsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
