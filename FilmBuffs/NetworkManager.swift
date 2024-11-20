eba8ae039cbe991d4927ae9fa7f612dfd4bd44a2
Clay Beal <125436817+clayster4004@users.noreply.github.com>
2024-11-19 23:58:13 -0500

    Added in actual information about who you are searching for in their respective views

--- FilmBuffs/NetworkManager.swift
+++ FilmBuffs/NetworkManager.swift
@@ -61,16 +61,92 @@ class NetworkManager {
     }
 
     struct TMDbSearchResult: Codable {
+        let id: Int
         let name: String?    // Actor name
         let title: String?   // Movie title
         let mediaType: String
+        let overview: String?
+        // let biography: String?
         
+        var description: String? {
+            switch mediaType {
+            case "person":
+                return "Biography Available upon Selection"
+            case "movie", "tv":
+                return overview
+            default:
+                return nil
+            }
+        }
         enum CodingKeys: String, CodingKey {
+            case id
             case name
             case title
             case mediaType = "media_type"
+            case overview
         }
     }
     
+    func fetchMovieDetails(movieId: Int, completion: @escaping (Result<MovieDetails, Error>) -> Void) {
+        let url = URL(string: "\(baseUrl)/movie/\(movieId)?api_key=\(apiKey)")!
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            guard let data = data else {
+                completion(.failure(NetworkError.noData))
+                return
+            }
+            do {
+                let movieDetails = try JSONDecoder().decode(MovieDetails.self, from: data)
+                completion(.success(movieDetails))
+            } catch {
+                completion(.failure(error))
+            }
+        }
+        task.resume()
+    }
     
+    func fetchActorDetails(actorId: Int, completion: @escaping (Result<ActorDetails, Error>) -> Void) {
+        let url = URL(string: "\(baseUrl)/person/\(actorId)?api_key=\(apiKey)")!
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            guard let data = data else {
+                completion(.failure(NetworkError.noData))
+                return
+            }
+            do {
+                let actorDetails = try JSONDecoder().decode(ActorDetails.self, from: data)
+                completion(.success(actorDetails))
+            } catch {
+                completion(.failure(error))
+            }
+        }
+        task.resume()
+    }
+    
+    func fetchTvShowDetails(tvShowId: Int, completion: @escaping (Result<TvShowDetails, Error>) -> Void) {
+        let url = URL(string: "\(baseUrl)/tv/\(tvShowId)?api_key=\(apiKey)")!
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            guard let data = data else {
+                completion(.failure(NetworkError.noData))
+                return
+            }
+            do {
+                let tvShowDetails = try JSONDecoder().decode(TvShowDetails.self, from: data)
+                completion(.success(tvShowDetails))
+            } catch {
+                completion(.failure(error))
+            }
+        }
+        task.resume()
+    }
 }
--- FilmBuffs/SearchView.swift
+++ FilmBuffs/SearchView.swift
@@ -5,80 +5,21 @@
 //  Created by Clay Beal on 11/14/24.
 //
 
-//import SwiftUI
-//
-//struct SearchView: View {
-//    @State private var searchText: String = ""
-//    @State private var suggestions = [String]()
-//    @State private var errorMessage: String?
-//    @EnvironmentObject var navi: MyNavigator
-//    
-////    private let allSuggestions = [
-////        "The Matrix", "Inception", "Leonardo DiCaprio", "Scarlett Johansson",
-////        "Tom Cruise", "Avengers", "The Dark Knight", "Natalie Portman",
-////        "Johnny Depp", "Meryl Streep", "Brad Pitt", "Fight Club"
-////    ]
-//    
-//    var body: some View {
-//        VStack {
-//            // Search Bar
-//            TextField("Search for movies or actors...", text: $searchText)
-//                .padding()
-//                .background(Color(.systemGray6))
-//                .cornerRadius(8)
-//                .padding(.horizontal)
-//                .onChange(of: searchText) {
-//                    performSearch(for: searchText)
-//                }
-//            
-//            List(suggestions, id: \.self) { suggestion in
-//                Text(suggestion)
-//                    .onTapGesture {
-//                        if isMovie(suggestion) {
-//                            navi.navigate(to: .AboutMovie)
-//                            print("Tapped")
-//                        } else {
-//                            navi.navigate(to: .AboutActor)
-//                        }
-//                    }
-//            }
-//            
-//            Spacer()  // Placeholder for the autofill suggestions and results
-//        }
-//        .navigationTitle("Search")
-//    }
-//    
-//    // Function to update suggestions based on search text
-//    private func updateSuggestions(for query: String) {
-//        if query.isEmpty {
-//            suggestions = []
-//        } else {
-//            suggestions = allSuggestions.filter { $0.localizedCaseInsensitiveContains(query) }
-//        }
-//    }
-//
-//    private func isMovie(_ suggestion: String) -> Bool {
-//        //return suggestion.contains("The") || suggestion.contains("Club") || suggestion.contains("Inception")
-//        return true
-//    }
-//}
-//
-//#Preview {
-//    SearchView()
-//}
-
 import SwiftUI
 
 struct SuggestionItem: Identifiable {
-    let id = UUID()
+    let id: Int
     let text: String
-    let mediaType: String  // Movie or person
+    let mediaType: String  // Will be "movie", "person", or "tv"
+    var description: String? // May be updated
+    var isLimitedInformation: Bool?
 }
 
 struct SearchView: View {
     @State private var searchText = ""                 // Holds user input
     @State private var suggestions = [SuggestionItem]()        // Holds fetched movie/actor names
     @State private var errorMessage: String?           // Error message, if any
+    @State private var debounceWorkItem: DispatchWorkItem?
     @EnvironmentObject var navi: MyNavigator           // Navigator for handling navigation
 
     var body: some View {
@@ -100,15 +41,35 @@ struct SearchView: View {
                     .padding()
             } else {
                 List(suggestions) { suggestion in
-                    Text(suggestion.text)
-                        .onTapGesture {
-                            if isMovie(suggestion) {
-                                navi.navigate(to: .AboutMovie)
-                            } else {
-                                navi.navigate(to: .AboutActor)
-                            }
+                    HStack {
+                        Text(suggestion.text)
+                        if suggestion.isLimitedInformation == true {
+                            Text("*")
+                                .foregroundColor(.red)
                         }
+                    }
+                    .onTapGesture {
+                        guard !suggestion.text.isEmpty else {
+                            print("Invalid Suggestion Detected")
+                            return
+                        }
+                        
+                        switch suggestion.mediaType {
+                        case "movie":
+                            navi.navigate(to: .AboutMovie(suggestion.id))
+                        case "person":
+                            navi.navigate(to: .AboutActor(suggestion.id))
+                        case "tv":
+                            navi.navigate(to: .AboutTvShow(suggestion.id))
+                        default:
+                            print("Unknown Media Type: \(suggestion.mediaType)")
+                        }
+                    }
                 }
+                Text("* May contain limited informaiton.")
+                    .font(.footnote)
+                    .foregroundColor(.gray)
+                    .padding(.top, 10)
             }
             
             Spacer()
@@ -116,66 +77,144 @@ struct SearchView: View {
         .navigationTitle("Search")
     }
     
-    // Function to perform the search
     private func performSearch(query: String) {
-        guard !query.isEmpty else {
-            suggestions = []  // Clear suggestions when search is empty
-            return
-        }
-        NetworkManager.shared.searchMoviesAndActors(query: query) { result in
+        // Cancel the previous work item if it exists
+        debounceWorkItem?.cancel()
+
+        // Create a new work item for debouncing
+        let workItem = DispatchWorkItem {
+            NetworkManager.shared.searchMoviesAndActors(query: query) { result in
                 DispatchQueue.main.async {
                     switch result {
                     case .success(let results):
-                        self.suggestions = results.map {
-                            SuggestionItem(text: $0.title ?? $0.name ?? "", mediaType: $0.mediaType)
+                        let validResults = results.filter { $0.title != nil || $0.name != nil }
+                        self.suggestions = validResults.map {
+                            var description: String?
+                            var isLimited: Bool?
+
+                            if $0.mediaType == "person" {
+                                // Biography will be fetched separately
+                                description = nil
+                                isLimited = nil
+                            } else if $0.mediaType == "movie" || $0.mediaType == "tv" {
+                                // Use the overview if available
+                                description = $0.overview
+                                isLimited = ($0.overview == nil || $0.overview!.isEmpty)
+                            }
+
+                            return SuggestionItem(
+                                id: $0.id,
+                                text: $0.title ?? $0.name ?? "Unknown",
+                                mediaType: $0.mediaType,
+                                description: description,
+                                isLimitedInformation: isLimited
+                            )
                         }
                         self.errorMessage = nil
+
+                        // Fetch additional details for items with limited information
+                        for suggestion in self.suggestions {
+                            if suggestion.mediaType == "person" {
+                                // Fetch biographies for actors
+                                fetchBiography(for: suggestion)
+                            } else if (suggestion.mediaType == "movie" || suggestion.mediaType == "tv") && suggestion.isLimitedInformation == true {
+                                // Fetch details for movies or TV shows
+                                fetchMediaDetails(for: suggestion)
+                            }
+                        }
                     case .failure(let error):
                         self.suggestions = []
                         self.errorMessage = error.localizedDescription
                     }
                 }
             }
-        
-//        NetworkManager.shared.searchMoviesAndActors(query: query, completion: { result in
-//            DispatchQueue.main.async(execute: {
-//                switch result {
-//                case .success(let results):
-//                    self.suggestions = results.map {
-//                        SuggestionItem(text: $0.title ?? $0.name ?? "", mediaType: $0.mediaType)
-//                    }
-//                    self.errorMessage = nil
-//                case .failure(let error):
-//                    self.suggestions = []
-//                    self.errorMessage = error.localizedDescription
-//                }
-//            })
-//        })
-        
-//        NetworkManager.shared.searchMoviesAndActors(query: query, completion) { result in
-//            DispatchQueue.main.async {
-//                switch result {
-//                case .success(let results):
-//                    self.suggestions = results.map {
-//                        SuggestionItem(text: $0.title ?? $0.name ?? "", mediaType: $0.mediaType)
-//                    }
-//                    self.errorMessage = nil
-//                case .failure(let error):
-//                    self.suggestions = []
-//                    self.errorMessage = error.localizedDescription
-//                }
-//            }
-//        }
-        
-        
+        }
+        // Assign and execute the work item after a delay
+        debounceWorkItem = workItem
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5, execute: workItem)
+    }
+
+    // Helper function to fetch actor biography
+    private func fetchBiography(for suggestion: SuggestionItem) {
+        NetworkManager.shared.fetchActorDetails(actorId: suggestion.id) { result in
+            DispatchQueue.main.async {
+                if let index = self.suggestions.firstIndex(where: { $0.id == suggestion.id }) {
+                    switch result {
+                    case .success(let actorDetails):
+                        if let biography = actorDetails.biography, !biography.isEmpty {
+                            self.suggestions[index].description = biography
+                            self.suggestions[index].isLimitedInformation = false
+                        } else {
+                            self.suggestions[index].description = "Biography not available"
+                            self.suggestions[index].isLimitedInformation = true
+                        }
+                    case .failure(let error):
+                        print("Error fetching actor details: \(error.localizedDescription)")
+                    }
+                }
+            }
+        }
+    }
+
+
+    private func fetchMediaDetails(for suggestion: SuggestionItem) {
+        if suggestion.mediaType == "movie" {
+            NetworkManager.shared.fetchMovieDetails(movieId: suggestion.id) { result in
+                DispatchQueue.main.async {
+                    if let index = self.suggestions.firstIndex(where: { $0.id == suggestion.id }) {
+                        switch result {
+                        case .success(let details):
+                            if let overview = details.overview, !overview.isEmpty {
+                                self.suggestions[index].description = overview
+                                self.suggestions[index].isLimitedInformation = false
+                            } else {
+                                self.suggestions[index].description = "Overview not available"
+                                self.suggestions[index].isLimitedInformation = true
+                            }
+                        case .failure(let error):
+                            print("Error fetching movie details: \(error.localizedDescription)")
+                        }
+                    }
+                }
+            }
+        } else if suggestion.mediaType == "tv" {
+            NetworkManager.shared.fetchTvShowDetails(tvShowId: suggestion.id) { result in
+                DispatchQueue.main.async {
+                    if let index = self.suggestions.firstIndex(where: { $0.id == suggestion.id }) {
+                        switch result {
+                        case .success(let details):
+                            if let overview = details.overview, !overview.isEmpty {
+                                self.suggestions[index].description = overview
+                                self.suggestions[index].isLimitedInformation = false
+                            } else {
+                                self.suggestions[index].description = "Overview not available"
+                                self.suggestions[index].isLimitedInformation = true
+                            }
+                        case .failure(let error):
+                            print("Error fetching TV show details: \(error.localizedDescription)")
+                        }
+                    }
+                }
+            }
+        }
     }
     
+    
+    private func suggestionHasLimitedData(_ suggestion: SuggestionItem) -> Bool {
+        return suggestion.isLimitedInformation ?? true
+    }
+        
+    
     private func isMovie(_ suggestion: SuggestionItem) -> Bool {
-        // Temporary logic to determine if suggestion is a movie or actor
-        // Ideally, we’d refine this once we have more detailed data
-//        return suggestion.contains("The") || suggestion.contains("Club") || suggestion == "Inception"
         return suggestion.mediaType == "movie"
     }
+    private func isPerson(_ suggestion: SuggestionItem) -> Bool {
+        return suggestion.mediaType == "person"
+    }
+    private func isTvShow(_ suggestion: SuggestionItem) -> Bool {
+        return suggestion.mediaType == "tv"
+    }
+    
 }
 
 #Preview {
