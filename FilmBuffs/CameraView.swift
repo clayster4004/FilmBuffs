b476aa8694706b1c36bd4d7f125ca22748f55764
Clay Beal <125436817+clayster4004@users.noreply.github.com>
2024-11-24 18:24:34 -0500

    Tweaked the api camera model and cleaned up the code a little

--- FilmBuffs/CameraView.swift
+++ FilmBuffs/CameraView.swift
@@ -8,8 +8,9 @@
 import SwiftUI
 
 struct CameraView: View {
-    @ObservedObject var viewModel: CameraViewModel // Connecting us to the ViewModel
-    
+    @ObservedObject var viewModel: CameraViewModel
+    @EnvironmentObject var navi: MyNavigator
+
     var body: some View {
         VStack {
             Button("Open Camera") {
@@ -21,19 +22,43 @@ struct CameraView: View {
                 Image(uiImage: image)
                     .resizable()
                     .scaledToFit()
-                    .frame(width: 200, height: 200) // For CoreML
+                    .frame(width: 200, height: 200) // Adjust as necessary
             }
+            // Display the cropped face image (if available)
+            if let croppedFace = viewModel.croppedFaceImage {
+                Text("Detected Face:")
+                    .padding(.top)
+                Image(uiImage: croppedFace)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 150, height: 150)
+                    .padding()
+            } else {
+                if viewModel.capturedImage != nil {
+                    Text("No face detected or still processing...")
+                        .foregroundColor(.gray)
+                        .padding(.top)
+                }
+            }
+
         }
-        
         .sheet(isPresented: $viewModel.isShowingCamera) {
             ImagePicker(sourceType: .camera) { image in
                 viewModel.handleCapturedImage(image)
             }
         }
+        .alert(isPresented: $viewModel.showConfirmationAlert) {
+            Alert(
+                title: Text("Is this \(viewModel.identifiedPersonName ?? "the person")?"),
+                primaryButton: .default(Text("Yes")) {
+                    viewModel.performManualSearch(navi: navi)  // Perform search if confirmed
+                },
+                secondaryButton: .cancel(Text("No"))
+            )
+        }
     }
 }
 
 
-//#Preview {
-//    CameraView(viewModel: CameraViewModel())
-//}
+
+
